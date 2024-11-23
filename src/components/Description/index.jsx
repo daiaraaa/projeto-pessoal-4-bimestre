501abcf84e3385d7638b4a974ac46319d799985f
Daiara Barroso <160050745+daiaraaa@users.noreply.github.com>
2024-11-22 21:48:27 -0300

    Update index.jsx

--- src/components/Description/index.jsx
+++ src/components/Description/index.jsx
@@ -4,7 +4,7 @@ export default function Description() {
   return (
     <section id="description" className="description">
       <div className="container">
-          <p>Bem vindo, sou estudante do curso de desenvolvimento de sistemas.</p>
+          <p>Bem vindo, sou estudante e faço curso de desenvolvimento de sistemas veja alguns dos meus projetos abaixos.</p>
       </div>
     </section>
   )
