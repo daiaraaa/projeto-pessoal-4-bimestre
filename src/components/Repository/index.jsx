f2480c98a08b3b235ab35361ef7a93dd478eb08c
Daiara Barroso <160050745+daiaraaa@users.noreply.github.com>
2024-11-26 18:43:57 -0300

    Update index.jsx

--- src/components/Repository/index.jsx
+++ src/components/Repository/index.jsx
@@ -5,13 +5,13 @@ export default function Repository() {
       <section id="repository" className="repository">
         <div className="container">
           <h2>Projetos no github:</h2>
-          <hr></hr>
+          <br></br>
           <div className="repository-grup">
             <div className="repository-proj">
               <h3>Projeto: Primeiro site em react</h3>
               <p>O meu primeiro site em react.</p>
               <a 
-                href="https://github.com/daiaraaa/projeto-1-primeiro-site-em-react" target="_blank" rel="noopener noreferrer"className="repository-link"
+                href="https://github.com/daiaraaa/projeto-1-primeiro-site-em-react" target="_blank" rel="noopener noreferrer"className="git"
               > Ver no GitHub </a>
             </div>
 
@@ -19,7 +19,7 @@ export default function Repository() {
               <h3>Projeto: Criando um componente em React</h3>
               <p>Criando um componente em react.</p>
               <a 
-                href="https://github.com/daiaraaa/projeto3-criando-um-componente-profile"  target="_blank" rel="noopener noreferrer" className="repository-link"
+                href="https://github.com/daiaraaa/projeto3-criando-um-componente-profile"  target="_blank" rel="noopener noreferrer" className="git"
               > Ver no GitHub </a>
             </div>
 
@@ -27,7 +27,7 @@ export default function Repository() {
               <h3>Projeto: Quiz história dinâmica</h3>
               <p>Um pequeno quiz sobre história geral.</p>
               <a 
-                href="https://github.com/daiaraaa/projeto-pessoal-utilizando-todos-os-conceitos" target="_blank" rel="noopener noreferrer" className="repository-link"
+                href="https://github.com/daiaraaa/projeto-pessoal-utilizando-todos-os-conceitos" target="_blank" rel="noopener noreferrer" className="git"
               > Ver no GitHub </a>
             </div>
 
@@ -35,7 +35,7 @@ export default function Repository() {
             <h3>Projeto: Site Black Clover</h3>
             <p>Um site que fala um pouco sobre o esquadrão dos Touros Negros do anime Black Clover. </p>
             <a 
-              href="https://github.com/daiaraaa/proj-prog-web-site-black-clover" target="_blank" rel="noopener noreferrer" className="repository-link"
+              href="https://github.com/daiaraaa/proj-prog-web-site-black-clover" target="_blank" rel="noopener noreferrer" className="git"
             > Ver no GitHub </a>
           </div>
           </div>
