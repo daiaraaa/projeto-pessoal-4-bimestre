4d416738f589332171a85c01e5a790ce942672e6
Daiara Barroso <160050745+daiaraaa@users.noreply.github.com>
2024-11-26 18:38:05 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,13 +4,8 @@ const Header = () => {
     <header className="header">
       <div className="container">
         <h1>Daiara Barroso</h1>
-        <nav>
-          <ul>
-            <li><a href="#description">Sobre</a></li>
-            <li><a href="#repository">Projetos</a></li>
-            <li><a href="#contact">Contato</a></li>
-          </ul>
-        </nav>
+        <br></br>
+        <p>Estudante programador</p>
       </div>
     </header>
   )
